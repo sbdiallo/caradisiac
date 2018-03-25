86d0e80166bb1e957c76e9c50253f1328ea609aa
sbdiallo <sorybineta.diallo@gmail.com>
2018-03-25 21:48:06 +0200

    feat(enddpoint /populate):  Indexing records to Elasticsearch

---
+++ populate.js
@@ -0,0 +1,43 @@
+/*Populate Endpoint: allows you to recover the necessary data using the node-car-api API and backups in an Elasticsearch database.*/
+const {getBrands} = require('node-car-api')
+const {getModels} = require('node-car-api')
+var elasticsearch = require('elasticsearch')
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+/*Return array of models for the given brand*/
+function addToElastic(model) {
+	client.create({
+        index: 'cars',
+        type: 'car',
+        id: model.uuid,
+        body: model
+      }, function (error, response) {
+        if(error)
+        {
+          console.log(error);
+        }
+    });
+}
+
+/*Return array of brands*/
+async function getBrandsList() {
+	var brandsList = await getBrands()
+	return brandsList;
+}
+
+module.exports = {
+	startPopulate : async function() {
+		getBrandsList().then((brands) => {
+			brands.forEach(async function(brand){
+				const models = await getModels(brand);
+				models.forEach(function(model){
+					addToElastic(model)
+				})
+				
+			})
+		})	
+	}
+}
