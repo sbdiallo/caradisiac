dd10e39bce31043f597af5f10140165bc2501f7c
sbdiallo <sorybineta.diallo@gmail.com>
2018-03-25 21:56:07 +0200

    feat(index.js):  Build the api

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const {getBrands} = require('node-car-api');
-const {getModels} = require('node-car-api');
+const express = require('express')
+const app = express()
+var populate = require('./populate')
 
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+  });
 
-
-
-
-
-
-async function getBrands(){
-	
-	return await getBrands();
-
-}
-
-async function getModelFromBrand(brand){
-	return await getModels(brand)
-}
-
-
-
-
-async function getModelsByBrands(brands) {
-    var models = [];
-    for (let brand of brands) {
-        //console.log(brand);
-        const model = await getModelFromBrand(brand);
-        model.push({
-            brand: brand,
-            model: model
-        });
-    }
-    return models;
-}
-
-
-getBrands().then(function(result){
-	getModelsByBrands(result).then(function(model){
-		console.log(model);
-	})
-	
+/*Populate Endpoint: allows you to recover the necessary data using the node-car-api API and backups in an Elasticsearch database.*/
+app.get('/populate', function (req, res) {
+	populate.startPopulate()
+  	res.send('Populate page')
 })
 
+/*Suv Endpoint: displays the data in descending order of volume*/
+app.get('/suv', function (req, res) {
+	let results = []
+    client.search({
+        index: 'cars',
+        type: 'car',
+        body: {
+            query: {
+               match_all: {},
+            },
+            sort: {
+                "volume.keyword": {
+                    order: "desc"
+                }
+            }
+        }
+      }).then(function (res) {
+            res.hits.hits.forEach(model => {
+                results.push(model['_source']);
+          });
+      }, function (err) {
+        console.trace(err.message);
+      }).then(() => {
+        res.json(results);
+      });
+})
 
-
+app.listen(9292, function () {
+  console.log('App is listening on port 9292!')
+})
